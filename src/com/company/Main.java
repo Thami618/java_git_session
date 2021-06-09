221de6a9b9e45991a9fb1fb43b313a9615e97327
Thami618 <thamngcobo1998@gmail.com>
2021-06-09 02:21:34 +0200

    done with ref and primitive

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int a =10;
+        int b = a;
+        a = 100;
+        System.out.println();
 
-        System.out.println("Java app a success");
-        System.out.println("test git");
-
-
+        Person alex = new Person("Alex");
+        Person mariam = alex;
+        System.out.println("B4 Changing alex");
+        System.out.println(alex.name + " " + mariam.name);
+        alex.name = "Alendra";
+        System.out.println("B4 Changing alex");
+        System.out.println(alex.name + " " + mariam.name);
+    }
+    static class Person{
+        String name;
+        Person(String name){
+            this.name = name;
+        }
     }
 }
